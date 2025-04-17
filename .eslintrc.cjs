a4926f1b30c48b83a2b4367a0545969412884bf0
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-04-17 02:13:34 -0300

    tailwind and linter config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,37 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'import', 'unused-imports', 'simple-import-sort'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
-  },
+    'import/named': 'error',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-cycle': 'error',
+    'unused-imports/no-unused-imports': 'warn',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: true }
+    ],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [['^react', '^next', '^[a-z]', '^@', '^\\.']]
+      }
+    ],
+    'simple-import-sort/exports': 'warn',
+    'react/prop-types': 'off',
+    'no-unused-vars': ['warn']
+  }
 }
